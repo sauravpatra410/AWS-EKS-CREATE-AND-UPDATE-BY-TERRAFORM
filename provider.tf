d9e19fd8804a1bd0243cc0e9ba727c268012e924
CloudTechDevOps <147053314+CloudTechDevOps@users.noreply.github.com>
2025-06-12 22:44:32 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -3,12 +3,3 @@ provider "aws" {
   region = var.region
 }
 
-# configure backend
-terraform {
-  backend "s3" {
-    bucket         = "test-lambda-events-bucket"
-    key            = "aws-eks-terraform.tfstate"
-    region         = "ap-south-1"
-    # dynamodb_table = "terraform_state"
-  }
-}
